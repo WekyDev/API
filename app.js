5bea06bbd455df4758bee5219a298b926aed9d74
Face <69168154+face-hh@users.noreply.github.com>
2021-09-02 16:49:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
 const express = require('express');
 
 require('dotenv').config();
-require('./src/Database/startup')(express);
+
+const mongoose = require('mongoose');
+const glob = require('glob');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const bodyparser = require('body-parser');
+
+// Mongoose
+mongoose.connect(process.env.mongo, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+}).then(() => {
+  console.log('Connected to MongoDB!');
+}).catch((err) => {
+  console.log(err);
+});
+
+app.use(bodyparser.json());
+// Handler
+app.use(cors({ exposedHeaders: ['Authorisation'] }));
+const load = async () => {
+  const files = glob.sync('./src/Routes/*.js');
+  files.forEach((route) => {
+    const file = require(`${path.resolve(route)}`);
+    app.use(`${file.endpoint}`, file.router);
+  });
+};
+
+app.use(express.static('public'));
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/stylesheet.css', (_req, res) => {
+  res.sendFile(process.cwd() + '/stylesheet.css')
+})
+load();
+app.listen(3000, function () {
+  console.log(`Weky API : ${3000}`);
+});
